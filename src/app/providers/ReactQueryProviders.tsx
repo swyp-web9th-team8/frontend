a61c8b3c5cb7788f56ae69d082c5d9fe661886cb
bhoh032019 <bhoh0320@ajou.ac.kr>
2025-04-15 02:50:51 +0900

    Refactor: reactquerydevtools 불필요한 조건부 렌더링 제거

--- src/app/providers/ReactQueryProviders.tsx
+++ src/app/providers/ReactQueryProviders.tsx
@@ -14,9 +14,7 @@ export default function ReactQueryProvider({
   return (
     <QueryClientProvider client={queryClient}>
       {children}
-      {process.env.NODE_ENV === 'development' && (
-        <ReactQueryDevtools initialIsOpen={false} />
-      )}
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
