6474175394f04f630a729e278f892f1ab600616d
bhoh032019 <bhoh0320@ajou.ac.kr>
2025-04-16 03:59:06 +0900

    Feat:axios 인스턴스 생성 및 공통 설정 적용

---
+++ src/lib/axiosInstance.ts
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_SERVER_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  timeout: 5000,
+});
+
+export default axiosInstance;
