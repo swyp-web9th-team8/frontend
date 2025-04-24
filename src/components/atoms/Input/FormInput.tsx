9979e3c61ae3e01d5e94346833ec6d8d6dd0d151
bhoh032019 <bhoh0320@ajou.ac.kr>
2025-04-24 17:38:52 +0900

    Feat: 공통 Input 컴포넌트 추가

---
+++ src/components/atoms/Input/FormInput.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { useFormContext } from "react-hook-form";
+
+interface FormInputProps {
+  name: string;
+  label?: string;
+  placeholder?: string;
+  type?: string;
+}
+
+const FormInput = ({
+  name,
+  label,
+  placeholder,
+  type = "text",
+}: FormInputProps) => {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext();
+
+  return (
+    <div className="flex flex-col gap-2">
+      {label && (
+        <label htmlFor={name} className="text-[#1A1A1A]">
+          {label}
+        </label>
+      )}
+      <input
+        id={name}
+        {...register(name)}
+        type={type}
+        placeholder={placeholder}
+        className="h-12 rounded-full border-[0.8px] border-[#B0B0B0] px-4 py-4"
+      />
+      {errors[name] && (
+        <span className="text-sm text-red-500">{`${errors[name]?.message}`}</span>
+      )}
+    </div>
+  );
+};
+
+export default FormInput;
