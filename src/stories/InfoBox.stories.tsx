e652de88d7c4b48924f86b58da7763992de85eb5
bhoh032019 <bhoh0320@ajou.ac.kr>
2025-05-02 14:24:50 +0900

    chore:파일명 변경을 위해 InfoBox.tsx를 임시로 temp.tsx로 변경

--- src/stories/InfoBox.stories.tsx
+++ src/stories/InfoBox.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import InfoBox from "../components/atoms/InfoBox/InfoBox";
+import InfoBox from "@/components/atoms/InfoBox/temp";
 
 const meta: Meta<typeof InfoBox> = {
   title: "Atoms/InfoBox",
