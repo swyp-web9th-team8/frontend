fe053f41e7434fd55f24be8b4c4d7cea4d3aee0a
bhoh032019 <bhoh0320@ajou.ac.kr>
2025-05-07 01:28:05 +0900

    Feat: 홈 피드 페이지 UI 설계

--- src/app/(routes)/home/page.tsx
+++ src/app/(routes)/home/page.tsx
@@ -1,103 +1,153 @@
-import Image from "next/image";
+"use client";
+
+import DateBadge from "@/components/atoms/Badge/DateBadge";
+import GatheringCreateButton from "@/components/atoms/Button/GatheringCreateButton";
+import { IGatheringItem } from "@/types/gatherings";
+import { getDay, getDayOfWeek, getMonth } from "@/utils/day";
+import React from "react";
+import IconSearch from "@/assets/icons/IconSearch.svg";
+import IconNotification from "@/assets/icons/IconNotification.svg";
+import IconClock from "@/assets/icons/clock.svg";
+import IconLocation from "@/assets/icons/location.svg";
+import IconDropdown from "@/assets/icons/dropdown-arrow.svg";
+
+const gatherings: IGatheringItem[] = [
+  {
+    postId: 1,
+    title: "아침 플로깅 함께해요!",
+    meetingTime: "2025-05-02T08:00:00",
+    placeName: "잠실 아크로 중앙정원",
+    address: "서울시 송파구 올림픽로 300",
+    participantCount: 3,
+  },
+  {
+    postId: 2,
+    title: "줍깅은 처음이라면 여기부터!",
+    meetingTime: "2025-05-02T09:00:00",
+    placeName: "잠실 아크로 중앙정원",
+    address: "서울시 송파구 올림픽로 300",
+    participantCount: 5,
+  },
+  {
+    postId: 3,
+    title: "초보자 환영 플로깅 모임",
+    meetingTime: "2025-05-02T08:00:00",
+    placeName: "잠실 아크로 중앙정원",
+    address: "서울시 송파구 올림픽로 300",
+    participantCount: 2,
+  },
+  {
+    postId: 4,
+    title: "주말 한강 플로깅",
+    meetingTime: "2025-05-03T08:00:00",
+    placeName: "한강공원",
+    address: "서울시 영등포구 여의동로 280",
+    participantCount: 4,
+  },
+  {
+    postId: 5,
+    title: "주말 아침 산책 겸 플로깅",
+    meetingTime: "2025-05-04T08:00:00",
+    placeName: "중앙공원",
+    address: "서울시 서초구 반포대로 210",
+    participantCount: 6,
+  },
+  {
+    postId: 6,
+    title: "친환경 플로깅 챌린지!",
+    meetingTime: "2025-05-04T08:00:00",
+    placeName: "서래마을 공원",
+    address: "서울시 서초구 서래로 50",
+    participantCount: 1,
+  },
+];
 
 export default function Home() {
-  return (
-    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
-      <main className="row-start-2 flex flex-col items-center gap-[32px] sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-center font-[family-name:var(--font-geist-mono)] text-sm/6 sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="rounded bg-black/[.05] px-1 py-0.5 font-[family-name:var(--font-geist-mono)] font-semibold dark:bg-white/[.06]">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const groupedByDate = gatherings.reduce<Record<string, IGatheringItem[]>>(
+    (acc, curr) => {
+      const date = curr.meetingTime.split("T")[0];
+      if (!acc[date]) acc[date] = [];
+      acc[date].push(curr);
+      return acc;
+    },
+    {},
+  );
 
-        <div className="flex flex-col items-center gap-4 sm:flex-row">
-          <a
-            className="bg-foreground text-background flex h-10 items-center justify-center gap-2 rounded-full border border-solid border-transparent px-4 text-sm font-medium transition-colors hover:bg-[#383838] sm:h-12 sm:w-auto sm:px-5 sm:text-base dark:hover:bg-[#ccc]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="flex h-10 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-4 text-sm font-medium transition-colors hover:border-transparent hover:bg-[#f2f2f2] sm:h-12 sm:w-auto sm:px-5 sm:text-base md:w-[158px] dark:border-white/[.145] dark:hover:bg-[#1a1a1a]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  return (
+    <div className="min-h-screen px-5 pt-[4.5rem] pb-28">
+      <div className="mb-9 flex items-center justify-between">
+        <div className="flex items-center gap-0.5">
+          <h1 className="font-gsans-medium text-heading2-medium text-grey-950">
+            서초동
+          </h1>
+          <button>
+            <IconDropdown className="h-6 w-6" />
+          </button>
         </div>
-      </main>
-      <footer className="row-start-3 flex flex-wrap items-center justify-center gap-[24px]">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="flex gap-4">
+          <button>
+            <IconSearch />
+          </button>
+          <button>
+            <IconNotification />
+          </button>
+        </div>
+      </div>
+
+      <div className="mb-3.5">
+        <button className="font-gsans-bold text-body1-bold text-green border-grey-200 mr-2 border-r-2 pr-2">
+          모집중인 모임
+        </button>
+        <button className="text-grey-200 font-gsans-bold text-body1-bold">
+          모집 완료된 모임
+        </button>
+      </div>
+
+      {Object.entries(groupedByDate).map(([date, items]) => (
+        <div key={date} className="mb-2.5 flex gap-2.5">
+          <div>
+            <DateBadge
+              month={getMonth(date)}
+              day={getDay(date)}
+              dayOfWeek={getDayOfWeek(date)}
+            />
+          </div>
+          <div className="flex w-full flex-col gap-3">
+            {items.map((gathering) => (
+              <div
+                key={gathering.postId}
+                className="flex justify-between rounded-2xl border border-gray-200 p-4 shadow-[0_4px_24px_rgba(170,170,170,0.15)]"
+              >
+                <div className="flex flex-col gap-3">
+                  <div className="font-gsans-bold text-body2-medium text-grey-950">
+                    {gathering.title}
+                  </div>
+                  <div className="flex flex-col gap-1">
+                    <div className="text-body2-medium font-gsans-medium text-grey-950 flex gap-2">
+                      <IconClock className="h-4 w-4" />
+                      <span>오전 8시</span>
+                    </div>
+                    <div className="text-body2-medium font-gsans-medium text-grey-950 flex gap-2">
+                      <IconLocation className="h-4 w-4" />
+                      <span>{gathering.placeName}</span>
+                    </div>
+                  </div>
+                </div>
+                <div className="flex flex-col gap-[1.375rem]">
+                  <span className="text-body3-medium font-gsans-medium text-grey-400">
+                    {gathering.participantCount}자리 남음
+                  </span>
+                  <button className="bg-green text-grey-0 font-gsans-medium text-body3-medium rounded-full px-3 py-2">
+                    참여하기
+                  </button>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      ))}
+
+      <GatheringCreateButton />
     </div>
   );
 }
