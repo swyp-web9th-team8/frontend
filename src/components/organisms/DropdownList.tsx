e3cf7560b4db906568458d463fc353902016dd5b
하주영 <ju980304@naver.com>
2025-05-07 03:18:50 +0900

    Feat: 드롭다운 인풋(InputWithDropdown) 구현

---
+++ src/components/organisms/DropdownList.tsx
@@ -0,0 +1,13 @@
+interface Props {
+  children: React.ReactNode;
+}
+
+function DropdownList({ children }: Props) {
+  return (
+    <div className="flex flex-col gap-4.5 rounded-xl border-[0.8px] border-[#B0B0B0] bg-white px-3.5 py-5">
+      {children}
+    </div>
+  );
+}
+
+export default DropdownList;
