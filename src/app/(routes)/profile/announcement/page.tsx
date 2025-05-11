7d82ec77ec4f465d3cdc8d18d834c5a9ec25015e
bhoh032019 <bhoh0320@ajou.ac.kr>
2025-05-11 16:30:33 +0900

    Feat: 공지사항 페이지 UI 구현

--- src/app/(routes)/profile/announcement/page.tsx
+++ src/app/(routes)/profile/announcement/page.tsx
@@ -1,5 +1,37 @@
+"use client";
+
+import Link from "next/link";
 import Header from "@/components/organisms/Header";
+import IconChevronRight from "@/assets/icons/IconChevronRight.svg";
+import { ANNOUNCEMENTS } from "@/data/announcements";
 
 export default function AnnouncementPage() {
-  return <Header title="공지사항" backButton />;
+  return (
+    <div>
+      <Header title="공지사항" backButton />
+      <ul className="flex flex-col">
+        {ANNOUNCEMENTS.map((item) => (
+          <li
+            key={item.id}
+            className="border-grey-200 border-b last:border-b-0"
+          >
+            <Link
+              href={`/profile/announcement/${item.id}`}
+              className="flex items-center justify-between py-3.5"
+            >
+              <div className="flex flex-col gap-1">
+                <span className="text-body3-medium text-grey-400">
+                  {item.date}
+                </span>
+                <p className="text-body2-medium font-gsans-medium text-grey-950">
+                  {item.title}
+                </p>
+              </div>
+              <IconChevronRight className="text-grey-400 h-6 w-6" />
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
