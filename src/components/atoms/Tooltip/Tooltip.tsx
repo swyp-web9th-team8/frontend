03746224e39b37a7226e9558bfc17c24e2f2e63e
bhoh032019 <bhoh0320@ajou.ac.kr>
2025-05-15 22:55:35 +0900

    Design: 로그인 페이지 Icon className 수정

--- src/components/atoms/Tooltip/Tooltip.tsx
+++ src/components/atoms/Tooltip/Tooltip.tsx
@@ -13,7 +13,7 @@ export default function Tooltip({ text }: TooltipProps) {
         <TooltipIcon />
       </div>
       <div className="invisible absolute bottom-full left-1/2 z-10 mb-2 w-max -translate-x-1/3 group-hover:visible">
-        <div className="text-body3-medium relative rounded-md bg-gray-950 px-[14px] py-3 text-xs text-white shadow">
+        <div className="text-body3-medium font-gsans-medium relative rounded-md bg-gray-950 px-[14px] py-3 text-xs text-white shadow">
           {text}
           <div className="absolute -bottom-1 left-1/3 h-2 w-2 -translate-x-1/2 rotate-45 bg-gray-950" />
         </div>
