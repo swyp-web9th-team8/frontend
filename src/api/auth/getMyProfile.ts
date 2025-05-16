9362db3f2a360a8f5c4b15bf7f0e590a68328201
bhoh032019 <bhoh0320@ajou.ac.kr>
2025-05-16 16:58:27 +0900

    Fix: getMyProfile requestHandler return값 수정

--- src/api/auth/getMyProfile.ts
+++ src/api/auth/getMyProfile.ts
@@ -1,6 +1,5 @@
 import { requestHandler } from "@/lib/axiosInstance";
 
 export const getMyProfile = async () => {
-  const response = await requestHandler("get", `/api/auth/me`);
-  return response.data;
+  return await requestHandler("get", `/api/auth/me`);
 };
