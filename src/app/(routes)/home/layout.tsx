a5231b6ba5331951613af0ea961e0b4e35ff83b3
하주영 <ju980304@naver.com>
2025-05-19 03:17:09 +0900

    Style: 홈 피드화면에서 데이터없을 경우에도 스크롤생기는 오류 수정

--- src/app/(routes)/home/layout.tsx
+++ src/app/(routes)/home/layout.tsx
@@ -3,7 +3,7 @@ import Nav from "@/components/organisms/Nav";
 function HomeLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-1 flex-col">
-      <div className="bottom-padding flex-1">{children}</div>
+      <div className="bottom-padding flex flex-1 flex-col">{children}</div>
       <Nav />
     </div>
   );
