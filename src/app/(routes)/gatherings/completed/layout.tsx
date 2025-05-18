292d90529bbb535d4c18eecbd2864090eb5b1bef
하주영 <ju980304@naver.com>
2025-05-18 15:57:54 +0900

    Fix: 헤더, 네비바 중복으로 보이는 오류 수정

--- src/app/(routes)/gatherings/completed/layout.tsx
+++ src/app/(routes)/gatherings/completed/layout.tsx
@@ -1,12 +1,7 @@
-import CompletedGatheringHeader from "@/components/organisms/header/CompletedGatheringHeader";
-import Nav from "@/components/organisms/Nav";
-
 function CompletedGatheringLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-1 flex-col">
-      <CompletedGatheringHeader title="지난 모임" backButton />
       <div className="bottom-padding flex-1">{children}</div>
-      <Nav />
     </div>
   );
 }
--- src/app/(routes)/gatherings/completed/page.tsx
+++ src/app/(routes)/gatherings/completed/page.tsx
@@ -1,12 +1,16 @@
+import CompletedGatheringHeader from "@/components/organisms/header/CompletedGatheringHeader";
+import Nav from "@/components/organisms/Nav";
 import GatheringCompletedTemplate from "@/components/templates/GatheringCompletedTemplate";
 
 export default function GatheringCompletedPage() {
   return (
     <div className="flex flex-col gap-5">
+      <CompletedGatheringHeader title="지난 모임" backButton />
       <h1 className="justify-start text-xl leading-normal font-bold text-[#1A1A1A]">
         동네에 이런 모임이 있었어요 🔥
       </h1>
       <GatheringCompletedTemplate />
+      <Nav />
     </div>
   );
 }
