575950eb055244dc8eb3cbd551db9f3cb97414ab
하주영 <ju980304@naver.com>
2025-05-19 13:28:32 +0900

    Feat: 콘솔 추가

--- src/components/atoms/Input/ImagesUploader.tsx
+++ src/components/atoms/Input/ImagesUploader.tsx
@@ -48,6 +48,8 @@ function ImagesUploader({ name, validationRules }: Props) {
           extension && ALLOWED_EXTENSIONS.includes(extension);
         const isValidSize = file.size <= MAX_FILE_SIZE;
 
+        console.log("size", file.size);
+
         if (!isValidExtension) {
           showToast(
             `${ALLOWED_EXTENSIONS.join(", ")} 외 파일형식은 지원되지 않아요`,
