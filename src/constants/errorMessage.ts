adcd8a6ff585c0f9f050235380b46c2aaed45409
하주영 <ju980304@naver.com>
2025-05-24 00:04:46 +0900

    Modify: 모임 목록 조회의 pos 필드에 '서울특별시' 추가

--- src/constants/errorMessage.ts
+++ src/constants/errorMessage.ts
@@ -1,4 +1,6 @@
 export const ERROR_MESSAGES = {
   NOT_SEOUL: "현재는 서울에서만 서비스 이용이 가능해요",
   LOCATION_FETCH_FAIL: "위치 정보를 가져오는데 실패했습니다.",
+  NOT_SATISFIED_LOCATION_PARAMETER:
+    "'OO구 OO대로(또는 '로' 또는 '길') 0' 형식으로 API 요청바랍니다.",
 };
--- src/hooks/queries/useCreateGathering.ts
+++ src/hooks/queries/useCreateGathering.ts
@@ -1,4 +1,6 @@
 import { createGathering } from "@/api/gatherings/gatherings";
+import { useToast } from "@/components/molecules/toast/ToastContext";
+import { ERROR_MESSAGES } from "@/constants/errorMessage";
 import {
   ICreateGatheringRequest,
   ICreateGatheringResponse,
@@ -7,8 +9,17 @@ import { useMutation } from "@tanstack/react-query";
 
 // TODO: 백엔드 postId 달라고 요청
 export const useCreateGathering = () => {
+  const showToast = useToast();
   return useMutation<ICreateGatheringResponse, Error, ICreateGatheringRequest>({
     mutationFn: createGathering,
-    onError: () => {},
+    onSuccess: (res) => {
+      if (
+        res.statusCode === "BAD_REQUEST" &&
+        res.message === ERROR_MESSAGES.NOT_SATISFIED_LOCATION_PARAMETER
+      ) {
+        showToast("주소 형식이 올바르지 않습니다");
+        return;
+      }
+    },
   });
 };
