ac102b6e343babd129baef5c0a764f58dbeecc2c
하주영 <ju980304@naver.com>
2025-05-23 23:46:45 +0900

    Fix: FAB 버튼 고정

--- src/components/atoms/Button/GatheringCreateButton.tsx
+++ src/components/atoms/Button/GatheringCreateButton.tsx
@@ -2,14 +2,32 @@
 
 import AddIcon from "@/assets/icons/add.svg";
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function GatheringCreateButton() {
   const { push } = useRouter();
+  const [right, setRight] = useState<number | undefined>(undefined);
+
+  useEffect(() => {
+    const updateRight = () => {
+      const width = window.innerWidth;
+      setRight(width > 500 ? (width - 500) / 2 + 21 : 21);
+    };
+
+    updateRight();
+    window.addEventListener("resize", updateRight);
+    return () => {
+      window.removeEventListener("resize", updateRight);
+    };
+  }, []);
 
   return (
     <button
-      className="bg-green fixed right-[18px] bottom-[96px] flex h-16 w-16 cursor-pointer items-center justify-center rounded-full shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]"
+      className={`bg-green fixed bottom-[96px] flex h-16 w-16 cursor-pointer items-center justify-center rounded-full shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] ${
+        right ? "opacity-100" : "opacity-0"
+      }`}
       onClick={() => push("/gatherings/create")}
+      style={{ right }}
     >
       <AddIcon className="h-8 w-8" />
     </button>
